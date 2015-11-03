f54618dbc2c4696a576ffa595826a73918c34e5d
JMatharu <jagdeepmatharu88@gmail.com>
2015-11-02 22:06:18 -0500

    Adding Read ME and Fixing Code

---
+++ README.md
@@ -0,0 +1,15 @@
+# Youtube Title and Name Extractor
+### Installation Guide
+#### Prerequisite
+1. Python [https://www.python.org/downloads/]
+2. PIP [http://pip.readthedocs.org/en/stable/installing/]
+
+## Running Script
+Simply download 'youtube-dl-links.py' and run following command
+```sh
+python youtube-dl-links.py
+```
+License
+----
+MIT
+**Free Software, Hell Yeah!**
--- youtube-dl-links.py
+++ youtube-dl-links.py
@@ -1,24 +1,29 @@
 __author__ = 'jmatharu'
 
+import os
+os.system('python -m pip install beautifulsoup4')
+os.system('python -m pip install requests')
 import requests
-import re
 from bs4 import BeautifulSoup
-
+    
 def youtube_link_extractor():
-    url = input("Paste Playlist URL : ")
+    url = input('Paste Playlist URL : ')
+    file_name = input('Enter File Name : ')
     url_filter = url.replace('https://www.youtube.com/playlist?','')
     url_index = '&&index='
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
     for link in soup.findAll('a', {'class': 'pl-video-title-link'}):
+        fw = open(file_name+'.txt', 'a')
         href = link.get('href')
         filtered_video_front = href.replace('/watch?v=', '')
         filtered_video_link = filtered_video_front.replace(url_filter,'')
         final_link_string = filtered_video_link.replace('&&index=', ' (Video Number) ----> ')
         final_link_string2 = final_link_string.replace('&index=', ' (Video Number) ----> ')
         title = link.string
-        print(title +" "+final_link_string2)
+        print(title +' '+final_link_string2+'\n')
+        fw.write(title +' '+final_link_string2+'\n')
+        fw.close()
 
 youtube_link_extractor()
-# &index=4&
